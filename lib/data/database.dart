3e6c453f0d01e2cffada92e5019e1563558b5d88
ibrahim YILDIZ <yildizib@gmail.com>
2026-08-08 03:26:11 +0300

    fix: database schema tweak

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -1,6 +1,6 @@
+import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
 
 class AppDatabase {
   static final AppDatabase instance = AppDatabase._internal();
